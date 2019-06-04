3e1cc235ab2ed5a67f7c695b8edad4ca9e55aba9
Michael <50213884+michaelxy11@users.noreply.github.com>
2019-06-04 18:32:13 +0200

    Added view model to navigation view
    Allows access to variables stored in data.js

---
+++ app/views/navigate/navigate-view-model.js
@@ -0,0 +1,14 @@
+var observableModule = require("tns-core-modules/data/observable");
+var data = require("../shared/data.js");
+
+function welcomeViewModel() {
+  var viewModel = observableModule.fromObject({
+    guest: data.guest,
+    restaurant: data.restaurant,
+    table: data.table
+  });
+
+  return viewModel;
+}
+
+module.exports = welcomeViewModel;
