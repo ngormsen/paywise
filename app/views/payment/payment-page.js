6638c8f502e584b32a8adb14880a90e6973e5df8
Michael <50213884+michaelxy11@users.noreply.github.com>
2019-07-04 13:59:38 +0200

    Paypal web view direct integration

--- app/views/payment/payment-page.js
+++ app/views/payment/payment-page.js
@@ -1,25 +1,35 @@
+// Imports
 const getFrameById = require("tns-core-modules/ui/frame").getFrameById;
 var data = require("../shared/data.js");
 const Observable = require("tns-core-modules/data/observable").Observable;
 var paymentViewModel = require("./payment-view-model");
 var observableModule = require("tns-core-modules/data/observable");
-
-//paypal
-
 var webViewInterfaceModule = require('nativescript-webview-interface');
 var oWebViewInterface;
 var firebase = require("nativescript-plugin-firebase");
-var data = require("../shared/data.js");
 var dialogs = require("tns-core-modules/ui/dialogs");
 var dataStore = require("../shared/data");
 
- 
+// Global variables
+viewModel = new Observable();
+var page;
+
+
+// Initialization (page load)
 exports.pageLoaded = pageLoaded;
 function pageLoaded(args){
     page = args.object;
+    page.bindingContext = viewModel;
+    // Show payment information
+    var sum = parseFloat(data.value).toFixed(2);
+    viewModel.set("sum", `Zu bezahlender Betrag: ${sum} EURO.`);
+    viewModel.set("restaurant", `${data.restaurant}`);
+    viewModel.set("points", `Du hast aktuell ${data.points} Punkte.`);
+    // Setup webView
     setupWebViewInterface(page)
 }
  
+
 // Initialize webView
 function setupWebViewInterface(page){
     var webView = page.getViewById('webView');
@@ -35,6 +45,7 @@ exports.webViewTouch = webViewTouch;
 function webViewTouch(args) {
     //console.log("log: web view touched");
 }
+
 
 // Handle events from webview (paypal)
 function handleEventFromWebView(){
@@ -96,39 +107,8 @@ function handleEventFromWebView(){
 }
 
 
-
-//Payment-page js
-
-viewModel = new Observable();
-
-function onPageLoaded(args) {
-    var sum = parseFloat(data.value).toFixed(2);
-    const page = args.object;
-    viewModel.set("sum", `Zu bezahlender Betrag: ${data.value} EURO.`);
-    viewModel.set("restaurant", `${data.restaurant}`);
-    viewModel.set("points", `Du hast aktuell ${data.points} Punkte.`)
-    // viewModel.on(Observable.propertyChangeEvent, (propertyChangeData) => {
-    //     console.log("observable change")
-    // })
-    page.bindingContext = viewModel;
-}
-exports.onPageLoaded = onPageLoaded;
-
-function onPaypalTap() {
-    const frame = getFrameById("topframe");
-    frame.navigate("views/paypal/paypal-page");
-}
-
-exports.onPaypalTap = onPaypalTap;
-
-function onGoogleTap() {
-    const frame = getFrameById("topframe");
-    frame.navigate("views/google-page/google-page");
-}
-
-exports.onGoogleTap = onGoogleTap;
-
-
+// Use points for payment
+exports.onPoints = onPoints;
 function onPoints(){
     let points = data.points;
     let value = points / 100 * 0.5
@@ -141,5 +121,16 @@ function onPoints(){
     console.log("onPoints", data.points)
 
 }
-exports.onPoints = onPoints;
 
+/* exports.onPaypalTap = onPaypalTap;
+function onPaypalTap() {
+    const frame = getFrameById("topframe");
+    frame.navigate("views/paypal/paypal-page");
+}
+
+
+exports.onGoogleTap = onGoogleTap;
+function onGoogleTap() {
+    const frame = getFrameById("topframe");
+    frame.navigate("views/google-page/google-page");
+} */
