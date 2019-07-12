1da6c521eb3e560370c9bfdce10449f29037089e
David <tdavidjin@gmail.com>
2019-07-12 15:26:01 +0200

    intro-table & check-out-page

--- app/views/Check-out/check-out-page.js
+++ app/views/Check-out/check-out-page.js
@@ -1,9 +1,17 @@
-exports.navigate = function() {
-//code transfer back to navigate-view
-}
 
+//code transfer back to navigate-view
+/*
+function onOrdersTap() {
+    const frame = getFrameById("topframe");
+    frame.navigate("views/orders/orders-page");
+}
+exports.onOrdersTap = onOrdersTap
+*/
+/*
 exports.SignOut = function(){
         firebase.logout()
         .then(console.log("user logged out"))
         alert("Du wurdest ausgeloggt")    
     }
+
+    */
